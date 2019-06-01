bcfa0e3176303c331628fefc8a91eb4fb615bfeb
Raghu Betina <raghu@firstdraft.com>
2019-06-01 21:16:13 +0000

    Direct association between buckets and favorites

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,6 +1,9 @@
 class Bucket < ApplicationRecord
   # Direct associations
 
+  has_many   :favorites,
+             :dependent => :destroy
+
   has_many   :activities,
              :dependent => :destroy
 
