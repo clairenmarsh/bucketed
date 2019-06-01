07d5fae055551b32487a181c59ce15390c3181e4
Raghu Betina <raghu@firstdraft.com>
2019-06-01 21:17:28 +0000

    Whitelist columns in Follower for ActiveAdmin

--- app/admin/followers.rb
+++ app/admin/followers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Follower do
+
+ permit_params :follower_user_id, :folowee_user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
