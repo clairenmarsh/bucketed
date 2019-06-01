9a1d02c74e184ad4a785763dca738c6329dd78be
Raghu Betina <raghu@firstdraft.com>
2019-06-01 21:16:13 +0000

    Direct association between locations and activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,8 @@
 class Activity < ApplicationRecord
   # Direct associations
 
+  belongs_to :location
+
   belongs_to :photo
 
   has_many   :comments,
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,9 @@
 class Location < ApplicationRecord
   # Direct associations
 
+  has_many   :activities,
+             :dependent => :destroy
+
   has_many   :users,
              :dependent => :destroy
 
