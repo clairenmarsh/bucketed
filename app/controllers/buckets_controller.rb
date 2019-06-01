64a241ee0f11769f8bad71cdde956512e2c93076
Raghu Betina <raghu@firstdraft.com>
2019-06-01 21:20:05 +0000

    Integrate Ransack

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,6 +1,7 @@
 class BucketsController < ApplicationController
   def index
-    @buckets = Bucket.page(params[:page]).per(10)
+    @q = Bucket.ransack(params[:q])
+    @buckets = @q.result(:distinct => true).includes(:user, :activities, :favorites, :photo, :location).page(params[:page]).per(10)
 
     render("bucket_templates/index.html.erb")
   end
